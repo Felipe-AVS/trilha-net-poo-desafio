b25f5159fde8fd4aa8119a2d9602fe3ba76a1b04
Felipe-AVS <127243970+Felipe-AVS@users.noreply.github.com>
2025-06-23 11:52:26 -0300

    Desafio concluído com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 64);
+System.Console.WriteLine("Nokia Criado com sucesso!");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+Iphone iphone = new Iphone("987654321", "iPhone 13", "987654321098765", 256);
+System.Console.WriteLine("Iphone Criado com sucesso!");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
